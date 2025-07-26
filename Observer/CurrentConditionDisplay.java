d1e96da87d9eff98a3362057a67e7743d6265752
Leon Sidelnikov <leonsidelnikov@gmail.com>
2025-07-26 15:33:15 +0300

    rewrite Observer using java.utils

--- Observer/CurrentConditionDisplay.java
+++ Observer/CurrentConditionDisplay.java
@@ -1,19 +1,27 @@
 package Observer;
 
+import java.util.Observable;
+import java.util.Observer;
+
+
 public class CurrentConditionDisplay implements Observer, DisplayElement {
+    Observable observable;
     private float temperature;
     private float humidity;
     private Subject weatherData;
 
-    public CurrentConditionDisplay (Subject weatherData) {
-        this.weatherData = weatherData;
-        weatherData.registerObserver(this);
+    public CurrentConditionDisplay (Observable observable) {
+        this.observable = observable;
+        observable.addObserver(this);
     }
 
-    public void update(float temperature, float humidity, float pressure) {
-        this.temperature = temperature;
-        this.humidity = humidity;
-        display();
+    public void update(Observable obs, Object arg) {
+        if (obs instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData)obs;
+            this.temperature = weatherData.getTemperature();
+            this.humidity = weatherData.getHumidity();
+            display();
+        }
     }
 
     public void display() {
